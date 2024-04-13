7d4f6476fc0ba3c9944e7da26e804cc4c59bf2be
Shubham Saurav <ssaurav28502@gmail.com>
2024-04-13 17:06:33 +0530

    login signup

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const userRoutes = require('./userRoutes');
+const userRoutes = require('./routes/userRoutes');
+const inventoryRoutes = require('./routes/inventoryRoutes');
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI)
@@ -10,9 +11,10 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => console.error(err))
 
 app.use(express.json())
-app.use('/api/users', userRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/analysis', inventoryRoutes);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, (err) => {
     if (err) {
         console.error(err);
