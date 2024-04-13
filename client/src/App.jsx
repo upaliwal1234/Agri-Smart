750512e208f61403235417096fce7ac101fdd4e8
utkarsh-goyal1 <ukgoyal123@gmail.com>
2024-04-13 16:13:56 +0530

    Weather and crop assessment components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 // import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import CropAssessmentDetails from './Components/CropAssessmentDetails'
+import WeatherInfo from './Components/WeatherInfo'
 function App() {
 
   return (
@@ -8,6 +10,8 @@ function App() {
       {/* <div>Home</div> */}
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/cropAssessmentDetails' element={<CropAssessmentDetails />} />
+        <Route path='/weatherInfo' element={<WeatherInfo />} />
       </Routes>
     </div>
   )
