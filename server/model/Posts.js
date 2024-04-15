a5b09c34ea91ce4ee6775c55e466b5fd9aeee0ad
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2024-04-15 22:47:37 +0530

    Post model update

--- server/model/Posts.js
+++ server/model/Posts.js
@@ -3,24 +3,27 @@ const mongoose = require('mongoose');
 const postsSchema = new mongoose.Schema({
     name: {
         type: String,
-        trim: true, 
+        trim: true,
         required: true
     },
     title: {
         type: String,
-        trim: true, 
+        trim: true,
         required: true
     },
     text: {
         type: String,
-        trim: true, 
+        trim: true,
         required: true
     },
-    reply: {
-        type: [],
-        ref: 'Reply'
-    }
-},{timeStamps:true});
+    reply: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Reply'
+        }
+    ]
+
+}, { timeStamps: true });
 
 const Posts = mongoose.model('Posts', postsSchema);
 module.exports = Posts;
